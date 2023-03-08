0a3d5f9ee4e51fcdbbabde0d4532668a8defc13b
Wendel Lopes <wendellopes@lealsistemas.com>
2023-03-08 13:31:45 -0300

    Inicio cards List

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
 void main() {
+  String jsonString = 
+  List<dynamic> jsonList = jsonDecode(jsonString)
   runApp(AppOLX());
 }
 
@@ -15,8 +18,7 @@ class AppOLX extends StatelessWidget {
             centerTitle: true,
             backgroundColor: Colors.purple,
             actions: [
-              IconButton(
-                  onPressed: () {}, icon: const Icon(Icons.favorite_outline)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.favorite_outline)),
               IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
             ],
             leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
@@ -27,26 +29,89 @@ class AppOLX extends StatelessWidget {
               scrollDirection: Axis.horizontal,
               children: <Widget>[
                 Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border(
+                          right: BorderSide(
+                        color: Colors.grey,
+                        width: 2,
+                      ))),
                   width: 160.0,
-                  color: Colors.red,
                   child: const Center(
-                  child: Text(
-                'Item 1',
-                style: TextStyle(fontSize: 18, color: Colors.white),
-              )),
+                      child: Text(
+                    'Região de Venda',
+                    style: TextStyle(fontSize: 20, color: Colors.purple),
+                  )),
                 ),
                 Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border(
+                          right: BorderSide(
+                        color: Colors.grey,
+                        width: 2,
+                      ))),
                   width: 160.0,
-                  color: Colors.blue,
                   child: const Center(
-                  child: Text(
-                'Item 2',
-                style: TextStyle(fontSize: 18, color: Colors.white),
-              )),
+                      child: Text(
+                    'Categoria',
+                    style: TextStyle(fontSize: 20, color: Colors.purple),
+                  )),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border(
+                          right: BorderSide(
+                        color: Colors.grey,
+                        width: 2,
+                      ))),
+                  width: 160.0,
+                  child: const Center(
+                      child: Text(
+                    'Filtro',
+                    style: TextStyle(fontSize: 20, color: Colors.purple),
+                  )),
                 ),
               ],
             ),
           )),
+    );
+  }
+}
+
+class meuCard extends StatelessWidget {
+  final int id;
+  final String imagemURL;
+  final String descricao;
+  final String valor;
+  final String dataEHora;
+  final String local;
+
+  meuCard({
+    required this.id,
+    required this.imagemURL,
+    required this.descricao,
+    required this.dataEHora,
+    required this.local,
+    required this.valor,
+  });
+
+  factory meuCard.fromJson(Map<String, dynamic> json) {
+    return meuCard(
+      id: json['id'],
+      imagemURL: json['imagemURL'],
+      descricao: json['descricao'],
+      valor: json['valor'],
+      dataEHora: json['dataEHora'],
+      local: json['local'],
+    ); 
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(children: [Image.asset(imagemURL)]),
     );
   }
 }
