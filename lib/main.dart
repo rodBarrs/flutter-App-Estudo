8de91ee38cdd57aa7288b1038259cca6a9dc827b
rodBarrs <rodrigosbarros51@gmail.com>
2023-03-08 00:13:16 -0300

    ajustando ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,52 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const AppOLX());
+  runApp(AppOLX());
 }
 
 class AppOLX extends StatelessWidget {
-  const AppOLX({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
-      home: const MyHomePage(title: 'LEAL SISTEMAS OLX '),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter = _counter + 2;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        centerTitle: true,
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.favorite_outline)),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-        ],
-        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
-      ),
-      body: SizedBox(
-        height: 100,
-        child: ListView(
-          scrollDirection: Axis.horizontal,
-          children: <Widget>[
-            const SizedBox(
-              width: 200,
-              child: const Center(
+      title: 'OLX Leal',
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('OLX LEAL'),
+            centerTitle: true,
+            backgroundColor: Colors.purple,
+            actions: [
+              IconButton(
+                  onPressed: () {}, icon: const Icon(Icons.favorite_outline)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+            ],
+            leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
+          ),
+          body: SizedBox(
+            height: 100,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                Container(
+                  width: 160.0,
+                  color: Colors.red,
+                  child: const Center(
                   child: Text(
                 'Item 1',
                 style: TextStyle(fontSize: 18, color: Colors.white),
               )),
+                ),
+                Container(
+                  width: 160.0,
+                  color: Colors.blue,
+                  child: const Center(
+                  child: Text(
+                'Item 2',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              )),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          )),
     );
   }
 }
