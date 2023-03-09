285be4f67a9abcdfacaa59349434299515ef60d5
rodBarrs <rodrigosbarros51@gmail.com>
2023-03-09 00:02:51 -0300

    avançando com cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
+import 'package:flutter/services.dart';
 
 void main() {
-  String jsonString = 
-  List<dynamic> jsonList = jsonDecode(jsonString)
   runApp(AppOLX());
 }
 
@@ -18,7 +17,8 @@ class AppOLX extends StatelessWidget {
             centerTitle: true,
             backgroundColor: Colors.purple,
             actions: [
-              IconButton(onPressed: () {}, icon: const Icon(Icons.favorite_outline)),
+              IconButton(
+                  onPressed: () {}, icon: const Icon(Icons.favorite_outline)),
               IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
             ],
             leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
@@ -75,12 +75,16 @@ class AppOLX extends StatelessWidget {
                 ),
               ],
             ),
+            child: Container(
+          color: Colors.grey[200], // cor de fundo
+          child: AnunciosList(), // lista de anúncios
+        ), 
           )),
     );
   }
 }
 
-class meuCard extends StatelessWidget {
+class Anuncio {
   final int id;
   final String imagemURL;
   final String descricao;
@@ -88,30 +92,80 @@ class meuCard extends StatelessWidget {
   final String dataEHora;
   final String local;
 
-  meuCard({
+  Anuncio({
     required this.id,
     required this.imagemURL,
     required this.descricao,
+    required this.valor,
     required this.dataEHora,
     required this.local,
-    required this.valor,
   });
 
-  factory meuCard.fromJson(Map<String, dynamic> json) {
-    return meuCard(
+  factory Anuncio.fromJson(Map<String, dynamic> json) {
+    return Anuncio(
       id: json['id'],
       imagemURL: json['imagemURL'],
       descricao: json['descricao'],
       valor: json['valor'],
       dataEHora: json['dataEHora'],
       local: json['local'],
-    ); 
+    );
   }
+}
+
+Future<List<Anuncio>> carregarAnuncios() async {
+  final response = await rootBundle.loadString('lib/anuncios.json');
+
+  final List<dynamic> json = jsonDecode(response);
+  return json.map((e) => Anuncio.fromJson(e)).toList();
+}
+
+class AnuncioCard extends StatelessWidget {
+  final Anuncio anuncio;
+
+  const AnuncioCard({Key? key, required this.anuncio}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(children: [Image.asset(imagemURL)]),
+      child: Column(
+        children: [
+          Image.network(anuncio.imagemURL),
+          ListTile(
+            title: Text(anuncio.descricao),
+            subtitle: Text('${anuncio.valor} - ${anuncio.dataEHora}'),
+            trailing: Text(anuncio.local),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AnunciosList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Anuncio>>(
+      future: carregarAnuncios(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final anuncios = snapshot.data!;
+          return ListView.builder(
+            itemCount: anuncios.length,
+            itemBuilder: (context, index) {
+              return AnuncioCard(anuncio: anuncios[index]);
+            },
+          );
+        } else if (snapshot.hasError) {
+          return Center(
+            child: Text('Erro ao carregar os anúncios'),
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 }
